5c657a5dbfdb1d06ad017fb3b14384577a56056e
Ravi Patel <ravifromchanga@gmail.com>
2026-02-17 12:38:14 +0530

    dev ip alloworgin added

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  allowedDevOrigins: ['172.16.12.129'],
+  allowedDevOrigins: ["172.16.12.129"],
   async rewrites() {
     return [
       {
-        source: '/index.php',
-        destination: '/',
+        source: "/index.php",
+        destination: "/",
       },
       {
-        source: '/contactform/contactform.php',
-        destination: '/api/contact',
+        source: "/contactform/contactform.php",
+        destination: "/api/contact",
       },
       {
-        source: '/ee/index.php',
-        destination: '/',
+        source: "/ee/index.php",
+        destination: "/",
       },
     ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
